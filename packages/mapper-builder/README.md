ae7ed2d2fd732b1774c8eff2c816fef2a1cc838a
zhaoyao91 <zhaoyao91@163.com>
2018-03-25 18:23:09 +0800

    finish mapper builder

--- packages/mapper-builder/README.md
+++ packages/mapper-builder/README.md
@@ -7,3 +7,22 @@ Help build mapper messager.
 - each mapper has a name prefixed with 'mapper', such as `mapper.$mapper`
 - each mapper uses its name as queue group name
 - it maps one message to another
+
+## API
+
+```
+(Options) => Void
+
+Options ~ {
+  natsEx: NatsEx,
+  name,
+  inTopic,
+  outTopic,
+  validator?: (data) => data,
+  mapData?: (data, message, receivedTopic) => Promise => data,
+}
+```
+
+## License
+
+MIT
--- packages/mapper-builder/index.js
+++ packages/mapper-builder/index.js
@@ -23,14 +23,12 @@ module.exports = function (options) {
   const fullName = `mapper.${name}`
 
   const wrapperHandler = async (data, message, receivedTopic) => {
+    data = validator ? validator(data) : data
     const result = await mapData(data, message, receivedTopic)
     message.emit(outTopic, result)
   }
 
-  natsEx.on(inTopic, wrapperHandler, {
-    validator,
-    queue: fullName,
-  })
+  natsEx.on(inTopic, wrapperHandler, {queue: fullName})
 }
 
 function id (x) {return x}
